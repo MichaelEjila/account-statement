c62b4b7c06e6441c8984ae598e8faed4964ad4b0
Michael Ejila <mac@Michaels-MBP.lan>
2022-09-12 14:36:47 +0100

    created functions and edited html

--- main/views.py
+++ main/views.py
@@ -2,18 +2,15 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.http import HttpResponse
+from datetime import datetime
 
 from .process import html_to_pdf 
 import json
 
-
-# Create your views here.
-def index(request):
-    #Retrieving Json Data
-    json_data = open('/Users/mac/Django/Bitnob/account_statement/main/data.json')
-
+def sort(json_data):
     #Loading json data into dictionary to pull out relevant data.
     parsed_json = json.load(json_data)
+
     
     #declaring dictionary to hold each transaction detail
     transData = {}
@@ -36,13 +33,15 @@ def index(request):
     #declaring dictionariy holding all transaction details to be passed into template
     response_dict = {}
 
+
     #populating dictionary with transactions
     for i in range(len(parsedData)):
         response_dict['Transaction{}'.format(i+1)]= parsedData[i]
+    return response_dict
 
+def convert(response_dict, user, opening_balance, closing_balance, date):
     #Context variable for template
-    context = {'data':response_dict}
-    print(response_dict)
+    context = {'data':response_dict, 'opening_balance':opening_balance,'closing_balance':closing_balance,'user': user, 'date': date}
     
     #rendering dynamic information into static html to be converted to pdf
     content = render_to_string('main/accounts.html', context)  
@@ -52,8 +51,21 @@ def index(request):
 
     #converting dynamic html into pdf
     pdf = html_to_pdf('main/statement.html')
+
+    return pdf
+
+# Create your views here.
+def index(request):
+    #Retrieving Json Data
+    json_data = open('/Users/mac/Django/Bitnob/account_statement/main/data.json')
+
+    #getting date
+    datetime1 = datetime.now() 
+    date1 = datetime1.date()
+
+    response_dict = sort(json_data)
+    pdf = convert(response_dict, 'Qala', 'Opening Balance', 'Closing Balance', date1)
     
-          
     #rendering pdf 
     return HttpResponse(pdf, content_type='application/pdf')
     
